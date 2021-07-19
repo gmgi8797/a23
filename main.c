96459084132340b287bfd374b51a0f34bbbf5c9d
DESKTOP-AMK6O7O\User <kkm2089@naver.com>
2021-07-19 15:06:29 +0900

    add j box

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 void main() {
-	int i;
+	int i,j;
 	for (i = 0; i < 10; i++) pritnf("%d %d %d\n", i, i * i, i + i);
+	for (j = 1; j < 10; j++) printf("%d %d %d\n", j, j * i, j + j + j);
 	printf("INNTINT\n");
 	printf("INAA\n");
 	printf("NIA\n");
